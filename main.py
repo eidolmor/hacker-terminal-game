e2632967f9667a9bcc9745791aa429f1767569f3
cookiesgit <cookiesgit@gmail.com>
2026-04-23 17:31:06 +0530

    Initial commit: add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def show_intro():
+    print("=" * 30)
+    print(" HACKER TERMINAL v1.0 ")
+    print("=" * 30)
+    print("Welcome, Agent.")
+    print("Your mission is to breach secure systems.\n")
+
+
+def main():
+    show_intro()
+    print("Game starting...\n")
+
+
+if __name__ == "__main__":
+    main()
